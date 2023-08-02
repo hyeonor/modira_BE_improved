1fb7dc6671901ac6cbfbd819e6c75efa68658ec5
hyeonorof <hyeonorof@gmail.com>
2023-08-02 15:19:50 +0900

    Rename: Like 엔티티의 컬럼 이름 변경

--- src/main/java/com/example/modiraa/model/Like.java
+++ src/main/java/com/example/modiraa/model/Like.java
@@ -14,15 +14,15 @@ public class Like {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "LIKE_ID", nullable = false)
+    @Column(name = "like_id", nullable = false)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "GIVER_USER_ID", nullable = false)
+    @JoinColumn(name = "giver_user_id", nullable = false)
     private Member giver;
 
     @ManyToOne
-    @JoinColumn(name = "RECEIVER_USER_ID", nullable = false)
+    @JoinColumn(name = "receiver_user_id", nullable = false)
     private Member receiver;
 
 
