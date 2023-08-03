0e1db4cb6fdc5ccdd615d75e9011eb83fdbcd7d3
hyeonorof <hyeonorof@gmail.com>
2023-08-03 21:46:13 +0900

    Refactor: application.yml에 spring.config.import 하는 방법으로 변경

--- src/main/java/com/example/modiraa/ModiraaApplication.java
+++ src/main/java/com/example/modiraa/ModiraaApplication.java
@@ -1,7 +1,7 @@
 package com.example.modiraa;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,19 +18,12 @@ public class ModiraaApplication {
         return new BCryptPasswordEncoder();
     }
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application-aws.yml,"
-            + "classpath:application.yml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ModiraaApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(ModiraaApplication.class, args);
     }
 
     @PostConstruct
     public void started(){
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-
     }
 }
