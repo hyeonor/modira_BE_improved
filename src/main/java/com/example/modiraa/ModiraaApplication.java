bf491bf8a510c243a690942dc8797c3109dfa1c0
hyeonorof <hyeonorof@gmail.com>
2023-08-05 09:05:26 +0900

    Refactor: BCryptPasswordEncoder @Bean 중복 등록 제거

--- src/main/java/com/example/modiraa/ModiraaApplication.java
+++ src/main/java/com/example/modiraa/ModiraaApplication.java
@@ -2,9 +2,7 @@ package com.example.modiraa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -12,11 +10,6 @@ import java.util.TimeZone;
 @EnableJpaAuditing
 @SpringBootApplication
 public class ModiraaApplication {
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ModiraaApplication.class, args);
