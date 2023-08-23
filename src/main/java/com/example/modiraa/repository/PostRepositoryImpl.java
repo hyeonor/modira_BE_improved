273b7fa0c1585ae26e49f2a5adaf352f99ca192f
hyeonorof <hyeonorof@gmail.com>
2023-08-23 22:56:10 +0900

    Refactor: 더보기 쿼리 메소드 개선

--- src/main/java/com/example/modiraa/repository/PostRepositoryImpl.java
+++ src/main/java/com/example/modiraa/repository/PostRepositoryImpl.java
@@ -58,17 +58,21 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
 
     @Override
     public Page<Post> findByIdLessThanAndCategory(Long lastId, String category, Pageable pageable) {
-        QueryResults<Post> result = queryFactory.selectFrom(post)
+        List<Post> result = queryFactory.selectFrom(post)
+                .join(post.postImage).fetchJoin()
+                .join(post.chatRoom).fetchJoin()
                 .where(post.id.lt(lastId).and(post.category.contains(category)))
-                .join(post.owner)
-                .join(post.postImage)
-                .join(post.chatRoom)
-                .fetchJoin()
                 .orderBy(post.id.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
-                .fetchResults();
-        return new PageImpl<>(result.getResults(), pageable, result.getTotal());
+                .fetch();
+
+        JPAQuery<Long> countQuery = queryFactory
+                .select(post.count())
+                .from(post)
+                .where(post.id.lt(lastId).and(post.category.contains(category)));
+
+        return PageableExecutionUtils.getPage(result, pageable, countQuery::fetchOne);
     }
 
     @Override
