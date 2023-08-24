f2c91aa4f8c99a61995531413b9b9c015e24555e
hyeonorof <hyeonorof@gmail.com>
2023-08-24 23:40:42 +0900

    Refactor: findByRoomCodeOrderByIdDesc 쿼리 메소드 개선

--- src/main/java/com/example/modiraa/repository/ChatMessageQueryRepository.java
+++ src/main/java/com/example/modiraa/repository/ChatMessageQueryRepository.java
@@ -2,14 +2,20 @@ package com.example.modiraa.repository;
 
 import com.example.modiraa.model.ChatMessage;
 import com.querydsl.core.QueryResults;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import static com.example.modiraa.model.QChatMessage.chatMessage;
+import static com.example.modiraa.model.QPost.post;
+import static com.example.modiraa.model.QPostImage.postImage;
 
 @RequiredArgsConstructor
 @Repository
@@ -17,14 +23,19 @@ public class ChatMessageQueryRepository {
     private final JPAQueryFactory queryFactory;
 
     public Page<ChatMessage> findByRoomCodeOrderByIdDesc(String roomCode, Pageable pageable) {
-        QueryResults<ChatMessage> result = queryFactory.selectFrom(chatMessage)
+        List<ChatMessage> result = queryFactory.selectFrom(chatMessage)
                 .where(chatMessage.roomCode.eq(roomCode))
                 .orderBy(chatMessage.id.desc())
-                .join(chatMessage.sender)
-                .fetchJoin()
+                .join(chatMessage.sender).fetchJoin()
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
-                .fetchResults();
-        return new PageImpl<>(result.getResults(), pageable, result.getTotal());
+                .fetch();
+
+        JPAQuery<Long> countQuery = queryFactory
+                .select(chatMessage.count())
+                .from(chatMessage)
+                .where(chatMessage.roomCode.eq(roomCode));
+
+        return PageableExecutionUtils.getPage(result, pageable, countQuery::fetchOne);
     }
 }
